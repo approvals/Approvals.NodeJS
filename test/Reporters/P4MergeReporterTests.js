8aed0ae183888a11ef70dcf84ac8078fd0b537be
Jason Jarrett <jason@elegantcode.com>
2012-12-19 12:09:02 -0800

    stubbed out p4merge reporter

---
+++ test/Reporters/P4MergeReporterTests.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var P4MergeReporter = require("../../lib/Reporters/P4MergeReporter.js");
+
+describe('Reporter', function () {
+	describe('p4merge', function () {
+		it('should use p4merge to report', function () {
+
+			var reporter = new P4MergeReporter();
+
+			var receivedFile = __dirname + "\\a.txt";
+			var approvedFile = __dirname + "\\b.txt";
+
+			reporter.report(approvedFile, receivedFile, function (command, args) {
+				assert.equal(command, "C:\\Program Files\\Perforce\\p4merge.exe");
+				assert.deepEqual(args, [ approvedFile, receivedFile ]);
+			});
+		})
+
+	})
+})
