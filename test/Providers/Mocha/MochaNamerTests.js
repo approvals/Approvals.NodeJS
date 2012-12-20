4983c5431bd0d52377f0ffc84e15e321ff11a73b
Jason Jarrett <jason@elegantcode.com>
2012-12-20 12:46:17 -0800

    Updated the MochaNamer to correctly pull from the mocha context and adhere to the Namer prototype.

--- test/Providers/Mocha/MochaNamerTests.js
+++ test/Providers/Mocha/MochaNamerTests.js
@@ -6,10 +6,18 @@ describe('MochaNamer', function () {
 
 	describe('when given a mocha context', function () {
 		it('should generate correct name', function () {
-			var namer = new MochaNamer(this);
 
-			assert.equal(namer.getFullTestName(), 
-				"MochaNamer.when_given_a_mocha_context.should_generate_correct_name");
+			var path = "C:\\temp\\";
+
+			var namer = new MochaNamer(this, path);
+
+			var approvedFileName = namer.getApprovedFile();
+			var receivedFileName = namer.getReceivedFile();
+
+			assert.equal(approvedFileName,
+				path + "MochaNamer.when_given_a_mocha_context.should_generate_correct_name.approved.txt");
+			assert.equal(receivedFileName,
+				path + "MochaNamer.when_given_a_mocha_context.should_generate_correct_name.received.txt");
 		});
 	});
 });
