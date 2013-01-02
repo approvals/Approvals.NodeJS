25b7ca1447c866358f51adc96ed71a9db77ce6ac
Jason Jarrett <jason@elegantcode.com>
2013-01-02 14:22:36 -0800

    more path specific fixes for linux

--- test/Providers/Mocha/MochaNamerTests.js
+++ test/Providers/Mocha/MochaNamerTests.js
@@ -1,4 +1,5 @@
 var assert = require("assert");
+var path = require('path');
 var MochaNamer = require("../../../lib/Providers/Mocha/MochaNamer.js");
 
 
@@ -7,20 +8,20 @@ describe('MochaNamer', function () {
 	describe('when given a mocha context', function () {
 		it('should generate correct name', function () {
 
-			var path = "C:\\temp\\";
-			var fileName = "C:\\temp\\foo.js";
+			var root = "C:\\temp\\";
+			var fileName = path.join("C:\\temp\\", "foo.js");
 
-			var namer = new MochaNamer(this, path, fileName);
+			var namer = new MochaNamer(this, root, fileName);
 
 			var approvedFileName = namer.getApprovedFile();
 			var receivedFileName = namer.getReceivedFile();
 
 			assert.equal(approvedFileName,
-				"C:\\temp\\" + 
-				"MochaNamer.when_given_a_mocha_context.should_generate_correct_name.approved.txt");
+				path.join("C:\\temp\\", 
+					"MochaNamer.when_given_a_mocha_context.should_generate_correct_name.approved.txt"));
 			assert.equal(receivedFileName,
-				"C:\\temp\\" + 
-				"MochaNamer.when_given_a_mocha_context.should_generate_correct_name.received.txt");
+				path.join("C:\\temp\\",
+					"MochaNamer.when_given_a_mocha_context.should_generate_correct_name.received.txt"));
 		});
 	});
 });
