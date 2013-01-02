ee6e70bd4406fe9fc779ada7065dea98e5afdb9b
Jason Jarrett <jason@elegantcode.com>
2013-01-02 15:38:23 -0800

    fixing windows path to git

--- test/Reporters/GitDiffReporterTests.js
+++ test/Reporters/GitDiffReporterTests.js
@@ -16,7 +16,7 @@ describe('Reporter', function () {
 			var expectedCommand;
 
 			if(process.platform.indexOf("win") !== -1) {
-				expectedCommand = "'C:/Program Files/Git/cmd/git.exe'";
+				expectedCommand = "'C:/Program Files/Git/cmd/git.cmd'";
 			}
 			else {
 				expectedCommand = "'/usr/bin/git'";
