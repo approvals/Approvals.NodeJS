d7213bb8b7883e646ae576731ecaa0912420c11b
Jason Jarrett <jason@elegantcode.com>
2013-01-15 20:57:37 -0800

    initial change to support configurable End of line when writing out files

--- test/StringWriterTests.js
+++ test/StringWriterTests.js
@@ -10,19 +10,47 @@ var fs = require('fs');
 
 describe('StringWriter', function () {
 
+  var defaultConfig = {};
+  
 	describe('', function () {
 		it('should default to type of txt', function () {
-			var stringWriter = new StringWriter("HELLO");
+			var stringWriter = new StringWriter(defaultConfig, "HELLO");
 			assert.equal(stringWriter.getFileExtension(), "txt");
 		});
 
 		it('should allow type to be specified', function () {
-			var stringWriter = new StringWriter("HELLO", "html");
+			var stringWriter = new StringWriter(defaultConfig, "HELLO", "html");
 			assert.equal(stringWriter.getFileExtension(), "html");
 		});
 
 		it('should write out file', function (done) {
-			var stringWriter = new StringWriter("HELLO");
+      var config = {};
+			var stringWriter = new StringWriter(config, "HELLO");
+
+			var settings = {
+				prefix: "foo",
+				suffix: "bar",
+				directory: __dirname
+			};
+
+			File.createTempFile(settings, function (error, file) {
+				if (error) throw error;
+				var filePath = file.getPath();
+
+				stringWriter.write(filePath);
+
+				fs.readFile(filePath, 'utf8', function (err, data) {
+					assert.equal(data, "HELLO");
+					done();
+				});
+			});
+    });
+
+    it('should write out file and append EOL', function (done) {
+      var config = {
+        appendEOL: true
+      };
+			var stringWriter = new StringWriter(config, "HELLO");
 
 			var settings = {
 				prefix: "foo",
