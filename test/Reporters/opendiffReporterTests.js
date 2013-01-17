36a3245606caa58933bb385773f5c2cb306afba5
Jason Jarrett <jason@elegantcode.com>
2013-01-16 21:59:58 -0800

    added the opendiff reporter

---
+++ test/Reporters/opendiffReporterTests.js
@@ -0,0 +1,27 @@
+var utils = require("../../lib/AUtils");
+var assert = require("assert");
+var path = require("path");
+var ReporterUnderTest = require("../../lib/Reporters/opendiffReporter.js");
+
+describe('Reporter', function () {
+
+	if (utils.findProgramOnPath('opendiff')) {
+		describe('opendiff', function () {
+			it('reporter args are correct', function () {
+
+				var reporter = new ReporterUnderTest();
+
+				var approvedFile = path.join(__dirname, "a.txt");
+				var receivedFile = path.join(__dirname, "b.txt");
+
+        assert.ok(reporter.report);
+				reporter.report(approvedFile, receivedFile, function (command, args) {
+
+					  assert.ok(command.toLowerCase().indexOf("opendiff") >= 0);
+					  assert.deepEqual(args, [receivedFile, approvedFile]);
+			    });
+
+			});
+		});
+	}
+});
