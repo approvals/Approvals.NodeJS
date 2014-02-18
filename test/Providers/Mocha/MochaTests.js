a443da9645908d2da0a8647a82b353f9a89f19b4
Jason Jarrett <jason@elegantcode.com>
2014-02-17 21:44:17 -0800

    Adding verityAsJSON - with Llewellyn.

--- test/Providers/Mocha/MochaTests.js
+++ test/Providers/Mocha/MochaTests.js
@@ -1,5 +1,6 @@
 require("../../../lib/Approvals").mocha(__dirname);
 
+
 describe('Mocha', function () {
 
     describe('when verifying some basic text', function () {
@@ -8,5 +9,13 @@ describe('Mocha', function () {
             this.verify("Hello World!");
 
         });
+
+        it('should verifyAsJSON', function () {
+
+            var value = {a:1, b:"bar"};
+
+            this.verifyAsJSON(value);
+
+        });        
     });
 });
