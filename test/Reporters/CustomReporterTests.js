7ebc343074a772c041ed26ebf3a9af72a44021d0
Jason Jarrett <jason@elegantcode.com>
2014-02-22 14:31:50 -0800

    ahh case sensitive

--- test/Reporters/CustomReporterTests.js
+++ test/Reporters/CustomReporterTests.js
@@ -18,7 +18,7 @@ var MyCustomReporter = function () {
 
 var globalCustomReporter = new MyCustomReporter();
 
-require('../../lib/approvals.js').configure({
+require('../../lib/Approvals').configure({
 	reporters: [globalCustomReporter]
 }).mocha(__dirname);
 
