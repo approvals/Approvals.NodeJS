f61c11e259228429555f9848757ae1509fabb31d
Jason Jarrett <jason@elegantcode.com>
2014-02-27 08:43:59 -0800

    Delete *.received.txt files when tests pass. Closes #10

--- test/FileApproverTests.js
+++ test/FileApproverTests.js
@@ -1,24 +1,63 @@
 'use strict';
+var assert = require('assert');
+var fs = require('fs');
 var Namer = require("../lib/Namer.js");
 var StringWriter = require("../lib/StringWriter.js");
-var DoNothingReporter = require("../lib/Reporters/donothingReporter.js");
 var FileApprover = require("../lib/FileApprover.js");
+
+var ShouldFailCustomReporter = function () {
+    return {
+        canReportOn: function (/*file*/) {
+            return true;
+        },
+        report: function (/*approved, received*/) {
+            throw "This reporter should never run";
+        },
+        name: "ShouldFailCustomReporter"
+    };
+};
+
 
 
 describe('FileApprover', function () {
 
-    describe('', function () {
-        it('should verify two files match', function () {
+    describe("when two files match", function(){
 
+        var namer;
+        var reporter;
+        var writer;
+        var config = { appendEOL: false };
+
+        beforeEach(function(){
             var dir = __dirname;
             var fileName = "FileApprover.should_verify_two_files_match";
-            var namer = new Namer(dir, fileName);
-            var config = { appendEOL: false };
-            var writer = new StringWriter(config, "HELLO!");
-            var reporter = new DoNothingReporter();
-
-            FileApprover.verify(namer, writer, reporter);
-
+            namer = new Namer(dir, fileName);
+            writer = new StringWriter(config, "HELLO!");
+            reporter = new ShouldFailCustomReporter();
         });
+
+        it('should verify two files match', function () {
+            FileApprover.verify(namer, writer, reporter);
+        });
+
+        it('should remove the received file', function () {
+            FileApprover.verify(namer, writer, reporter);
+            var receivedFileName = namer.getReceivedFile(writer.getFileExtension());
+
+            assert.ok(!fs.existsSync(receivedFileName), "Received File should be deleted");
+        });
+
+
+
+        it('should fail the approver if the writer gives something different', function () {
+
+            writer = new StringWriter(config, "BYE");
+
+            assert.throws(function() {
+                FileApprover.verify(namer, writer, reporter);
+            });
+        });
+
     });
+
 });
