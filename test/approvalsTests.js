ab2c82709407c47b875ffcd14cbf3ab9fcba3f4f
Jason Jarrett <jason@elegantcode.com>
2014-04-24 09:47:52 -0700

    trying gitdiff to see output in travis-ci

--- test/approvalsTests.js
+++ test/approvalsTests.js
@@ -6,7 +6,7 @@ describe('approvals', function () {
         it('can verify some manual text', function () {
             var testName = "manualVerification";
             var dataToVerify =  "some stuff here";
-            var reporters = []; //blank for now Need to work round the global unit test reporter problem...
+            var reporters = ["gitdiff"]; //blank for now Need to work round the global unit test reporter problem...
             approvals.verify(__dirname, testName, dataToVerify, reporters);
         });
     });
@@ -15,7 +15,7 @@ describe('approvals', function () {
         it('can verify some manual text', function () {
             var testName = "manualVerificationAsJSON";
             var dataToVerify =  { x: "some stuff here", y: 123, z: new Date(2011, 11, 1)};
-            var reporters = []; //blank for now Need to work round the global unit test reporter problem...
+            var reporters = ["gitdiff"]; //blank for now Need to work round the global unit test reporter problem...
             approvals.verifyAsJSON(__dirname, testName, dataToVerify, reporters);
         });
     });
