16fe57262f9bbceef84b0776df0c005228130128
Jason Jarrett <jason@elegantcode.com>
2014-04-24 08:01:07 -0700

    supporting a manually named test

---
+++ test/approvalsTests.js
@@ -0,0 +1,22 @@
+var approvals = require("../lib/Approvals.js");
+
+describe('approvals', function () {
+
+    describe('verify', function () {
+        it('can verify some manual text', function () {
+            var testName = "manualVerification";
+            var dataToVerify =  "some stuff here";
+            var reporters = ["VisualStudio"]; //blank for now Need to work round the global unit test reporter problem...
+            approvals.verify(__dirname, testName, dataToVerify, reporters);
+        });
+    });
+
+    describe('verifyAsJSON', function () {
+        it('can verify some manual text', function () {
+            var testName = "manualVerificationAsJSON";
+            var dataToVerify =  { x: "some stuff here", y: 123, z: new Date(2011, 11, 1)};
+            var reporters = ["VisualStudio"]; //blank for now Need to work round the global unit test reporter problem...
+            approvals.verifyAsJSON(__dirname, testName, dataToVerify, reporters);
+        });
+    });
+});
