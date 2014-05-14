7aa4ab316887cbec893013b785714985eaa462c1
Jason Jarrett <jason@elegantcode.com>
2014-05-13 21:36:22 -0700

    initial gulp test and lint commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 
-var testPaths = ['test/**/*[Tt]ests.js']
+var paths = {
+    mochaTests: ['test/**/*[Tt]ests.js'],
+    jasmineTests: ['test/**/*.Spec.js'],
+}
 
-var gulpMocha = gulp.src(testPaths, { read: false })
-    .pipe(mocha({
-        reporter: 'spec',
-        slow: 200,
-        timeout: 1000,
-        globals: { }
-    }));
+gulp.task('jshint', function(){
+	return gulp.src(["lib/**/*.js", "test/**/*.js"])
+	    .pipe(jshint())
+		.pipe(jshint.reporter('default'))
+});    
 
-gulp.task('default', function() {
-  // place code for your default task here
-  return gulpMocha;
-});
+gulp.task('test', function(){
+	return gulp.src(paths.mochaTests, { read: false })
+	    	.pipe(mocha({
+		        reporter: 'spec',
+		        slow: 500,
+		        timeout: 2000,
+		        globals: { }
+		    }));
+});    
+
+
+gulp.task('default', ["test", "jshint"]);
