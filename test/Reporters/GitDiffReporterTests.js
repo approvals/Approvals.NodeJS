9b6697b0a82b51d23e65c7ecec3db470c73edbb7
Jason Jarrett <jason@elegantcode.com>
2014-06-14 14:21:56 -0700

    fixing issue with test when git is in a different path

--- test/Reporters/GitDiffReporterTests.js
+++ test/Reporters/GitDiffReporterTests.js
@@ -14,25 +14,17 @@ describe('Reporter', function () {
             var approved = path.join(__dirname, "a.txt");
             var received = path.join(__dirname, "b.txt");
 
-            var expectedCommand;
-
-            if (os.platform.isWindows) {
-                expectedCommand = "'C:/Program Files/Git/cmd/git.exe'";
-            }
-            else {
-                expectedCommand = "'/usr/bin/git'";
-            }
-
-            expectedCommand += " diff --no-index -- " + received + " " + approved;
+            var expectedCommand = "'git' diff --no-index -- " + received + " " + approved;
 
             reporter.report(approved, received, function (command) {
 
-                // TODO: find a way to fix up the linux/mac 'git path' instead of hacking by replaces
                 var pathTrimmedCommand = command
-					.replace("git.cmd", "git.exe") // cmd is good enough
-                    .replace(" (x86)", "")       // Win x86 folder trim
-					.replace("local/git/", '');  // Git was installed here on the mac
+                  .replace("git.cmd'", "git")
+                  .replace("git.exe'", "git")
+                ;
 
+                var startTrim = pathTrimmedCommand.indexOf("git' diff")
+                pathTrimmedCommand = "'" + pathTrimmedCommand.substr(startTrim);
 
                 assert.equal(pathTrimmedCommand, expectedCommand);
                 return {};
