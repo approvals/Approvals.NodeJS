d1647120551534507fd5adf698295666650b02e3
Jason Jarrett <jason@elegantcode.com>
2015-04-16 15:55:31 -0700

    add a jscs task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var jshint = require('gulp-jshint');
+var $ = require('gulp-load-plugins')();
 
 var paths = {
     mochaTests: ['test/**/*[Tt]ests.js'],
     jasmineTests: ['test/**/*.Spec.js'],
-}
+    filesToLint: ['./lib/**/*.js', './test/**.js', 'gulpfile.js']
+};
 
-gulp.task('jshint', function(){
-	return gulp.src(["lib/**/*.js", "test/**/*.js"])
-	    .pipe(jshint())
-		.pipe(jshint.reporter('default'))
-});    
+gulp.task('jscs', function() {
+  return gulp.src(paths.filesToLint)
+    .pipe($.jscs());
+});
+
+gulp.task('jscs-watch', function() {
+  gulp.start('jscs');
+  $.watch(paths.filesToLint, function () {
+    gulp.start('jscs');
+  });
+});
+
+gulp.task('jshint', function() {
+  return gulp.src(["lib/**/*.js", "test/**/*.js"])
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'));
+});
 
 gulp.task('test', function(){
-	return gulp.src(paths.mochaTests, { read: false })
-	    	.pipe(mocha({
-		        reporter: 'spec',
-		        slow: 500,
-		        timeout: 2000,
-		        globals: { }
-		    }));
-});    
-
+  return gulp.src(paths.mochaTests, { read: false })
+    .pipe($.mocha({
+        reporter: 'spec',
+        slow: 500,
+        timeout: 2000,
+        globals: { }
+    }));
+});
 
 gulp.task('default', ["test", "jshint"]);
