a54c1399a7d74af798af0132c56bcbb531b702d5
Jason Jarrett <jason@elegantcode.com>
2015-04-18 13:25:50 -0700

    fix test description

--- test/commandLineTests.js
+++ test/commandLineTests.js
@@ -3,17 +3,15 @@ var path = require('path');
 
 describe('Command Line', function () {
 
-  describe('searchForExecutable', function () {
-    it('Should run ./commandTest.sh', function (done) {
-      var cliTestCommand = 'echo "Hello\nWorld" | ' + path.join('./', 'bin', 'index.js') + ' --reporter gitdiff --outdir ./test commandlineTest';
+  it('Should run approvals CLI with basic text input', function (done) {
+    var cliTestCommand = 'echo "Hello\nWorld" | ' + path.join('./', 'bin', 'index.js') + ' --reporter gitdiff --outdir ./test commandlineTest';
 
-      shelljs.exec(cliTestCommand, {async:true}, function (code, output) {
-        if (code !== 0) {
-          console.error('code:', code, 'output:', output);
-          throw "cli script failed";
-        }
-        done();
-      });
+    shelljs.exec(cliTestCommand, {async:true}, function (code, output) {
+      if (code !== 0) {
+        console.error('code:', code, 'output:', output);
+        throw "cli script failed";
+      }
+      done();
     });
   });
 
