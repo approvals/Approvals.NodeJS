c5b6e81a5749099734ceaffa52e5d54c56f9f7c4
Aswin <vaswin91@gmail.com>
2016-05-21 14:03:24 +0530

    adding vimdiff reporter tests and fix jscs warnings

---
+++ test/Reporting/Reporters/VimDiffReporterTests.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var expect = require("chai").expect;
+var path = require("path");
+var ReporterUnderTest = require("../../../lib/Reporting/Reporters/vimdiffReporter.js");
+
+
+describe('Reporter', function () {
+  describe('vimdiff', function () {
+    it('reporter args are correct', function () {
+
+      var reporter = new ReporterUnderTest();
+
+      var approved = path.join(__dirname, "a.txt");
+      var received = path.join(__dirname, "b.txt");
+
+
+      reporter.report(approved, received, function (command,arglist) {
+
+        var pathTrimmedCommand = command
+          .replace("vim.cmd", "vim")
+          .replace("vim.exe", "vim")
+          .toLowerCase();
+        var startTrim = pathTrimmedCommand.indexOf("vim");
+        pathTrimmedCommand =  pathTrimmedCommand.substr(startTrim);
+        expect(pathTrimmedCommand).to.equal("vim");
+        expect(arglist).to.deep.equal(['-d',received,approved]);
+        return {};
+      });
+    });
+
+  });
+});
