890b3472c184d51edb980ee8ba73d05be4336171
Jason Jarrett <jason@elegantcode.com>
2017-02-28 22:30:21 -0800

    extra tests as well as refactor generic DiffReporter

--- test/Reporting/Reporters/P4MergeReporterTests.js
+++ test/Reporting/Reporters/P4MergeReporterTests.js
@@ -1,36 +1,57 @@
 'use strict';
 
-var os = require("../../../lib/osTools");
 var assert = require("assert");
 var path = require("path");
 var ReporterUnderTest = require("../../../lib/Reporting/Reporters/p4mergeReporter.js");
+//var sinon = require('sinon');
 
 describe('Reporter', function () {
 
-  if (os.platform.isWindows) {
-    describe('p4merge', function () {
-      it('reporter args are correct', function () {
-        this.timeout(60000); // test runs slow on appveyor?
-        var reporter = new ReporterUnderTest();
+  // it("sample p4merge real test", function () {
+  //   var approvals = require('../../../lib/Approvals');
+  //   approvals.verify(__dirname, 'p4mergeTempTest', 'some test data', {
+  //     reporters: [
+  //       'p4merge'
+  //     ]
+  //   });
+  // });
 
-        var approvedFile = path.join(__dirname, "a.txt");
-        var receivedFile = path.join(__dirname, "b.txt");
+  describe('p4merge', function () {
+    it.skip('reporter args are correct', function () {
+      this.timeout(60000); // test runs slow on appveyor?
+      var reporter = new ReporterUnderTest();
 
-        if (reporter.canReportOn(receivedFile)) {
-          reporter.report(approvedFile, receivedFile, function (command, args) {
+      var approvedFile = path.join(__dirname, "a.txt");
+      var receivedFile = path.join(__dirname, "b.txt");
 
-            assert.equal(command.toLowerCase(), "c:/program files/perforce/p4merge.exe");
-            assert.deepEqual(args, [receivedFile, approvedFile]);
+      if (reporter.canReportOn(receivedFile)) {
+        reporter.report(approvedFile, receivedFile, function (command, args) {
 
-            return {
-              stdout: { on: function () { } },
-              stderr: { on: function () { } }
-            };
+          assert.equal(command.toLowerCase(), "c:/program files/perforce/p4merge.exe");
+          assert.deepEqual(args, [receivedFile, approvedFile]);
 
-          });
-        }
+          return {
+            stdout: { on: function () { } },
+            stderr: { on: function () { } }
+          };
 
+        });
+      }
+
+    });
+
+    describe('p4merge searchForExecutable', function () {
+
+      describe('on windows', function () {
+        it('should find it in Program Files');
+        it('should find it in Program Files (x86)');
+      });
+
+      describe('on mac', function () {
+        it('should find it in /Applications/p4merge.app/Contents/Resources/launchp4merge');
+        it('should find it in /opt/homebrew-cask/Caskroom/p4merge/2014.3-1007540/p4merge.app/Contents/Resources/launchp4merge');
       });
     });
-  }
+  });
+
 });
