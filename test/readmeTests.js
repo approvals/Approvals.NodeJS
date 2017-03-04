0c26afab7e210e42abb4372a584f9e6a2ac09c4d
Jason Jarrett <jason@elegantcode.com>
2017-03-03 22:15:18 -0800

    a test that auto-generates docs in the readme

---
+++ test/readmeTests.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var fs = require('fs');
+
+var FileApprover = require('../lib/FileApprover');
+var StringWriter = require("../lib/StringWriter");
+var ReporterFactory = require("../lib/Reporting/ReporterFactory");
+var approvals = require('../lib/Approvals');
+var jsdoc2md = require('jsdoc-to-markdown');
+
+describe("Readme", function () {
+
+  it("Should not allow the readme docs to get out of sync", function () {
+
+    var currentReadme = fs.readFileSync(path.join(__dirname, '../', 'readme.md')).toString()
+
+    var approvalsSource = fs.readFileSync(path.join(__dirname, '../lib', 'Approvals.js')).toString();
+
+    var newDocs = "<!--BEGIN-API-DOCS-->";
+    newDocs += "\n<!-- GENERATED - DO NOT MODIFY API DOCS IN THIS README -->";
+    newDocs += "\n<!-- Update docs in the source ./lib/Approvals.js -->";
+    newDocs += jsdoc2md.renderSync({ source: approvalsSource});
+    newDocs += "\n<!--END-API-DOCS-->";
+
+    var resultingReadme = currentReadme.replace(/<!--BEGIN-API-DOCS-->[\s\S]*<!--END-API-DOCS-->/gm, newDocs);
+
+    var config = approvals.getConfig();
+
+    var writer = new StringWriter(config, resultingReadme);
+    var namer = {
+      getReceivedFile: function () { return path.join(__dirname, '..', "readme.received.md"); },
+      getApprovedFile: function () { return path.join(__dirname, '..', "readme.md"); }
+    }
+
+    var reporterFactory = function () {
+      return [ReporterFactory.loadReporter(config.reporters)];
+    };
+
+    FileApprover.verify(namer, writer, reporterFactory, config);
+  });
+
+});
