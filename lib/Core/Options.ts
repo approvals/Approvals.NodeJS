6e6375f9065967e403fb9292e75edae3ce363e74
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-29 14:15:05 +0200

    B!! fixed bug with Options.modify

--- lib/Core/Options.ts
+++ lib/Core/Options.ts
@@ -1,54 +1,61 @@
 import {Scrubber} from "../Scrubbers/Scrubbers";
 
 class FileOptions {
-  private options: Options;
-  constructor(options: Options) {
-    this.options = options;
-  }
+    private options: Options;
 
-  withFileExtention(extensionWithDot: string): Options {
-    return this.options.modify("FileExtention", extensionWithDot);
+    constructor(options: Options) {
+        this.options = options;
+    }
 
-  }
+    withFileExtention(extensionWithDot: string): Options {
+        return this.options.modify("FileExtention", extensionWithDot);
 
-  getFileExtension(): string {
-    return this.options.get("FileExtention", () => ".txt")
-  }
+    }
+
+    getFileExtension(): string {
+        return this.options.get("FileExtention", () => ".txt")
+    }
 }
 
 export class Options {
-  protected fields: { [n: string]: any };
-  constructor() {
-    this.fields = {}
-  }
+    protected fields: { [n: string]: any };
 
-  modify(key: string, value: any): Options {
-    const next = new Options();
-    for (const key1 in next.fields) {
-      next.fields[key1] = this.fields[key1]
+    constructor() {
+        this.fields = {}
     }
-    next.fields[key] = value;
-    return next;
-  }
-  forFile(): FileOptions {
-    return new FileOptions(this);
-  }
 
-  get<Type>(key: string, default1: () => Type): Type {
-    if (this.fields[key] == undefined) {
-      return default1();
+    modify(key: string, value: any): Options {
+        const next = new Options();
+        for (const key1 in this.fields) {
+            next.fields[key1] = this.fields[key1]
+        }
+        next.fields[key] = value;
+        return next;
     }
-    return this.fields[key]
 
-  }
+    forFile(): FileOptions {
+        return new FileOptions(this);
+    }
 
-    withScrubber(scrubber: Scrubber) {
+    get<Type>(key: string, default1: () => Type): Type {
+        if (this.fields[key] == undefined) {
+            return default1();
+        }
+        return this.fields[key]
+
+    }
+
+    withScrubber(scrubber: Scrubber): Options {
         return this.modify("Scrubber", scrubber);
     }
+
     getScrubber(): Scrubber {
-    return this.get("Scrubber", () => t => t);
+        return this.get("Scrubber", () => t => t);
     }
+
     scrub(text: string): string {
-      return this.getScrubber()(text);
+        const scrubber = this.getScrubber();
+        const code = "" + scrubber;
+        return scrubber(text);
     }
 }
