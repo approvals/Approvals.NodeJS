04f68d76eaa13e6cb62085e494a0e8097b9aaf2f
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-10-29 13:37:11 +0200

    t** working on guid scrubbers

--- lib/Scrubbers/Scrubbers.ts
+++ lib/Scrubbers/Scrubbers.ts
@@ -1,1 +1,28 @@
 export type Scrubber = (t: string) => string;
+
+function _replaceRegex(text: string, regex: string, replacement: (index: number) => string): string {
+    const capturedGuids: string[] = []
+
+    const result = text.replace(regex, match => {
+        if (capturedGuids.indexOf(match) < 0) {
+            capturedGuids.push(match);
+        }
+
+        const index = capturedGuids.indexOf(match) + 1;
+        return replacement(index);
+    });
+    return result;
+}
+
+export class Scrubbers {
+
+    static createReqexScrubber(regex: string, replacement: (index: number) => string | string): Scrubber {
+        return t => _replaceRegex(t, regex, replacement)
+    }
+
+    static scrubAllGuids(text: string):string  {
+        let scrubber = this.createReqexScrubber("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}",
+            t => `<guid_${t}>`);
+        return scrubber(text);
+    }
+}
--- test/Providers/Jest/Scrubbers.test.ts
+++ test/Providers/Jest/Scrubbers.test.ts
@@ -3,14 +3,29 @@ import {verify, verifyAsJson} from "../../../lib/Providers/Jest/JestApprovals";
 import {convertToFilename} from "../../../lib/Providers/Jest/JestNamer";
 import {Options} from "../../../lib/Core/Options";
 import {StringUtils} from "../../../lib/Utilities/StringUtils";
-
+import {Scrubbers} from "../../../lib/Scrubbers/Scrubbers";
 
 
 describe("Scubbers", () => {
     test("scrub", () => {
         const text = "[1, 2, 12, 21, 121, 131, 222]";
 
-        verify(text,new Options().withScrubber(t => StringUtils.replaceAll(t, "2", "two")));
+        verify(text, new Options().withScrubber(t => StringUtils.replaceAll(t, "2", "two")));
+    });
+    test('guids', () => {
+        const text = {
+            people: [{id: "58f471f1-8b1f-413c-8971-21cb23bfc8f2", name: "Eve"},
+                {
+                    id: "24775957-3125-42db-ac6d-0abf142685b9",
+                    name: "Kane",
+                    mother: "58f471f1-8b1f-413c-8971-21cb23bfc8f2"
+                }, {
+                    id: "45365d5b-6cfc-430c-b842-36fbcd21470e",
+                    name: "Kane",
+                    mother: "58f471f1-8b1f-413c-8971-21cb23bfc8f2"
+                }]
+        };
+        verifyAsJson(text, new Options().withScrubber(Scrubbers.scrubAllGuids));
     });
 
 });
