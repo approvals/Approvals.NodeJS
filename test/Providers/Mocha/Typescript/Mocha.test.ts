58d7d9f8ffb5f0f1e5d2b5e13c14d71ee1351822
Lada Kesseler <lada.kesseler1@t-mobile.com>
2024-03-05 08:40:38 -0800

    . t mocha/typescript

---
+++ test/Providers/Mocha/Typescript/Mocha.test.ts
@@ -0,0 +1,13 @@
+//require('approvals').mocha();
+
+const assert = require('assert');
+
+describe("Mocha Typescript Approvals", () => {
+
+  it("verify", () => {
+    // verify("Hello From Approvals");
+    assert.equal(2, 2);
+  });
+});
+
+
