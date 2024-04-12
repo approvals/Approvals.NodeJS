dfa783a1dddad8aca8d184316ddafe6cad3d1de6
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-04-12 13:54:45 -0600

    - r modernized typescript
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- lib/Providers/BeforeEachVerifierBase.ts
+++ lib/Providers/BeforeEachVerifierBase.ts
@@ -1,55 +1,65 @@
-'use strict';
+import * as _ from 'lodash';
+import * as fs from 'fs';
+import * as cfg from '../config';
+import StringWriter from "../StringWriter.js";
+import FileApprover from "../FileApprover.js";
+import ReporterFactory from "../Reporting/ReporterFactory.js";
+import * as aUtils from '../AUtils';
 
-var _ = require('lodash');
-var fs = require('fs');
-var cfg = require('../config');
-var StringWriter = require("../StringWriter.js");
-var FileApprover = require("../FileApprover.js");
-var ReporterFactory = require("../Reporting/ReporterFactory.js");
-var aUtils = require('../AUtils');
+interface ApprovalsExtras {
+    getCurrentReporters(options?: any): any[];
+}
 
-module.exports = function (Namer, usageSample, dirName) {
+interface VerifyFunction {
+    (data: any, overrideOptions?: any): void;
+}
 
-  if (!fs.existsSync(dirName)) {
-    fs.mkdirSync(dirName);
-  }
+interface TestCaseContext {
+    approvals: ApprovalsExtras;
+    verify: VerifyFunction;
+    verifyAsJSON: VerifyFunction;
+}
 
-  // Make sure it's a valid directory
-  var stats = fs.lstatSync(dirName);
-  if (!stats.isDirectory()) {
-    throw new Error("Invalid directory [" + dirName + "]. Try using the following syntax. > " + usageSample);
-  }
+export = function (Namer: any, usageSample: string, dirName: string) {
+    if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName);
+    }
 
-  beforeEach(function () {
+    // Make sure it's a valid directory
+    const stats = fs.lstatSync(dirName);
+    if (!stats.isDirectory()) {
+        throw new Error(`Invalid directory [${dirName}]. Try using the following syntax. > ${usageSample}`);
+    }
 
-    var approvalsExtras = {
-      getCurrentReporters: function (options) {
-        options = options || cfg.currentConfig();
-        var reporterCandidates = ReporterFactory.loadAllReporters(options.reporters);
-        return reporterCandidates;
-      }
-    };
+    beforeEach(function (this: TestCaseContext) {
+        const approvalsExtras: ApprovalsExtras = {
+            getCurrentReporters: function (options?: any) {
+                options = options || cfg.currentConfig();
+                const reporterCandidates = ReporterFactory.loadAllReporters(options.reporters);
+                return reporterCandidates;
+            }
+        };
 
-    // Tack on an approvals property so we can add on some
-    // helper approvals goo this is mostly used for the test.
-    this.approvals = approvalsExtras;
+        // Tack on an approvals property so we can add on some
+        // helper approvals goo this is mostly used for the test.
+        this.approvals = approvalsExtras;
 
-    this.verify = function (data, overrideOptions) {
-      var namer = new Namer(this, dirName);
+        this.verify = function (data: any, overrideOptions?: any) {
+            const namer = new Namer(this, dirName);
 
-      var newOptions = _.defaults(overrideOptions || {}, cfg.currentConfig());
+            const newOptions = _.defaults(overrideOptions || {}, cfg.currentConfig());
 
-      var reporterFactory = function () {
-        return approvalsExtras.getCurrentReporters(newOptions);
-      };
+            const reporterFactory = function () {
+                return approvalsExtras.getCurrentReporters(newOptions);
+            };
 
-      var writer = new StringWriter(newOptions, data);
-      FileApprover.verify(namer, writer, reporterFactory);
-    };
+            const writer = new StringWriter(newOptions, data);
+            FileApprover.verify(namer, writer, reporterFactory);
+        };
 
-    this.verifyAsJSON = function (data, overrideOptions) {
-      this.verify(aUtils.stringifyKeysInOrder(data), overrideOptions);
-    };
+        this.verifyAsJSON = function (data: any, overrideOptions?: any) {
+            this.verify(aUtils.stringifyKeysInOrder(data), overrideOptions);
+        };
 
-  });
+    });
 };
