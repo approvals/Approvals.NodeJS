b49b788a53469d9e563c713bc7151639a4f46ff1
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-04-26 13:29:15 -0600

    . r extract method
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- lib/Providers/BeforeEachVerifierBase.ts
+++ lib/Providers/BeforeEachVerifierBase.ts
@@ -20,6 +20,37 @@ interface TestCaseContext {
     verifyAsJSON: VerifyFunction;
 }
 
+function beforeEachLoaderFunction(Namer: any, dirName: string, that: any): void {
+    const approvalsExtras: ApprovalsExtras = {
+        getCurrentReporters: function (options?: any) {
+            options = options || cfg.currentConfig();
+            const reporterCandidates = ReporterFactory.loadAllReporters(options.reporters);
+            return reporterCandidates;
+        }
+    };
+
+    // Tack on an approvals property so we can add on some
+    // helper approvals goo this is mostly used for the test.
+    that.approvals = approvalsExtras;
+
+    that.verify = function (data: any, overrideOptions?: any) {
+        const namer = new Namer(this, dirName);
+
+        const newOptions = _.defaults(overrideOptions || {}, cfg.currentConfig());
+
+        const reporterFactory = function () {
+            return approvalsExtras.getCurrentReporters(newOptions);
+        };
+
+        const writer = new StringWriter(newOptions, data);
+        FileApprover.verify(namer, writer, reporterFactory);
+    };
+
+    that.verifyAsJSON = function (data: any, overrideOptions?: any) {
+        this.verify(aUtils.stringifyKeysInOrder(data), overrideOptions);
+    };
+
+}
 export function beforeEachVerifierBase(Namer: any, usageSample: string, dirName: string) :void {
     if (!fs.existsSync(dirName)) {
         fs.mkdirSync(dirName);
@@ -30,38 +61,9 @@ export function beforeEachVerifierBase(Namer: any, usageSample: string, dirName:
     if (!stats.isDirectory()) {
         throw new Error(`Invalid directory [${dirName}]. Try using the following syntax. > ${usageSample}`);
     }
+    beforeEach(function () {
+        return beforeEachLoaderFunction(Namer, dirName, this);
+    });
 
-    beforeEach(beforeEachLoaderFunction);
 
-    function beforeEachLoaderFunction() {
-        const approvalsExtras: ApprovalsExtras = {
-            getCurrentReporters: function (options?: any) {
-                options = options || cfg.currentConfig();
-                const reporterCandidates = ReporterFactory.loadAllReporters(options.reporters);
-                return reporterCandidates;
-            }
-        };
-
-        // Tack on an approvals property so we can add on some
-        // helper approvals goo this is mostly used for the test.
-        this.approvals = approvalsExtras;
-
-        this.verify = function (data: any, overrideOptions?: any) {
-            const namer = new Namer(this, dirName);
-
-            const newOptions = _.defaults(overrideOptions || {}, cfg.currentConfig());
-
-            const reporterFactory = function () {
-                return approvalsExtras.getCurrentReporters(newOptions);
-            };
-
-            const writer = new StringWriter(newOptions, data);
-            FileApprover.verify(namer, writer, reporterFactory);
-        };
-
-        this.verifyAsJSON = function (data: any, overrideOptions?: any) {
-            this.verify(aUtils.stringifyKeysInOrder(data), overrideOptions);
-        };
-
-    }
 };
