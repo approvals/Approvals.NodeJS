9f9c07c709362aef20558b88cd887e7e2d279f15
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-04-27 22:16:02 -0600

    - r moving to ts

--- test/Reporting/AggregateDiffReporterTests.js
+++ test/Reporting/AggregateDiffReporterTests.js
@@ -12,9 +12,7 @@ describe('AggregateReporter', function () {
   it("should be able to report on a txt file", function () {
     this.timeout(60000); // just a test to see if it can pass on AppVeyor?
     var file = path.join(__dirname, "a.txt");
-
     var reporters = ReporterFactory.ReporterFactory.loadAllReporters(textDiffReporters);
-
     var reporterX = new DiffReporterAggregate(reporters);
     var canReportOn = reporterX.canReportOn(file);
     assert.ok(canReportOn, "Can report on " + file);
