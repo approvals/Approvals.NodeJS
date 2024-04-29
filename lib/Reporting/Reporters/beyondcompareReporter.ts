afab6628d40ef7b5fb359a0b71542ea62dcf12e6
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-04-29 09:45:56 -0600

    - r revert beyond compare typescript changes
    Co-Authored-By: Ilyas Landikov <93825870+ilandikov@users.noreply.github.com>

--- lib/Reporting/Reporters/beyondcompareReporter.ts
+++ lib/Reporting/Reporters/beyondcompareReporter.ts
@@ -1,30 +1,33 @@
-import {searchForExecutable} from '../../AUtils';
-import {platform} from '../../osTools';
-import * as shelljs from 'shelljs';
-import GenericDiffReporterBase from '../GenericDiffReporterBase';
+var autils = require('../../AUtils');
+var osTool = require('../../osTools');
+var shelljs = require('shelljs');
+var GenericDiffReporterBase = require('../GenericDiffReporterBase');
 
 class Reporter extends GenericDiffReporterBase {
-    constructor() {
-        super("BeyondCompare");
 
-        let app: string | null = null;
-        if (platform.isMac) {
-            try {
-                const results = shelljs.ls('/Applications/Beyond Compare.app/Contents/MacOS/bcomp');
-                app = results[0];
-            } catch (err) {
-                console.error(err);
-            }
-            app = app || autils.searchForExecutable("bcomp");
-        } else if (platform.isWindows) {
-            app = searchForExecutable("Beyond Compare 4", "BCompare.exe")
-                || searchForExecutable("Beyond Compare 3", "BCompare.exe");
-        }
+  constructor() {
 
-        app = app || searchForExecutable("bcomp");
+    super("BeyondCompare");
 
-        this.exePath = app;
+    var app = null;
+    if (osTool.platform.isMac) {
+      try {
+        app = shelljs.ls('/Applications/Beyond Compare.app/Contents/MacOS/bcomp')[0];
+      } catch (err) {
+        console.error(err);
+      }
+      app = app || autils.searchForExecutable("bcomp");
+    } else if (osTool.platform.isWindows) {
+      app = autils.searchForExecutable("Beyond Compare 4", "BCompare.exe")
+        || autils.searchForExecutable("Beyond Compare 3", "BCompare.exe");
     }
+
+    app = app || autils.searchForExecutable("bcomp");
+
+    this.exePath = app;
+
+  }
+
 }
 
 module.exports = Reporter;
