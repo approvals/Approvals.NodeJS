097250872b1e5c5b8b9909cfa86eb99cbb791367
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-06-14 13:27:16 -0600

    . e added prettier
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- test/Reporting/AggregateDiffReporterTests.ts
+++ test/Reporting/AggregateDiffReporterTests.ts
@@ -1,4 +1,4 @@
-import {ReporterFactory} from "../../lib/Reporting/ReporterFactory";
+import { ReporterFactory } from "../../lib/Reporting/ReporterFactory";
 
 import assert from "assert";
 
@@ -7,19 +7,18 @@ import path from "path";
 import DiffReporterAggregate from "../../lib/Reporting/DiffReporterAggregate";
 
 describe("AggregateReporter", function () {
-    const textDiffReporters = ["gitdiff", "p4merge"];
+  const textDiffReporters = ["gitdiff", "p4merge"];
 
-    it("should be able to report on a txt file", function () {
-        this.timeout(60000); // just a test to see if it can pass on AppVeyor?
-        const file = path.join(__dirname, "a.txt");
-        const reporters =
-            ReporterFactory.loadAllReporters(textDiffReporters);
-        const reporterX = new DiffReporterAggregate(reporters);
-        const canReportOn = reporterX.canReportOn(file);
-        assert.ok(canReportOn, "Can report on " + file);
+  it("should be able to report on a txt file", function () {
+    this.timeout(60000); // just a test to see if it can pass on AppVeyor?
+    const file = path.join(__dirname, "a.txt");
+    const reporters = ReporterFactory.loadAllReporters(textDiffReporters);
+    const reporterX = new DiffReporterAggregate(reporters);
+    const canReportOn = reporterX.canReportOn(file);
+    assert.ok(canReportOn, "Can report on " + file);
 
-        const foundReporter = reporterX.getReporter(file);
+    const foundReporter = reporterX.getReporter(file);
 
-        assert.ok(foundReporter, "foundReporter not found?");
-    });
+    assert.ok(foundReporter, "foundReporter not found?");
+  });
 });
--- test/Reporting/GenericDiffReporterBaseTests.ts
+++ test/Reporting/GenericDiffReporterBaseTests.ts
@@ -1,42 +1,42 @@
 import fs from "fs";
-import {expect} from "chai";
+import { expect } from "chai";
 import GenericDiffReporterBase from "../../lib/Reporting/GenericDiffReporterBase";
 import sinon from "sinon";
 
 describe("GenericDiffReporterBase", function () {
-    let sandbox;
-    let reporter;
+  let sandbox;
+  let reporter;
 
-    beforeEach(function () {
-        sandbox = sinon.createSandbox();
-        reporter = new GenericDiffReporterBase("test");
+  beforeEach(function () {
+    sandbox = sinon.createSandbox();
+    reporter = new GenericDiffReporterBase("test");
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  describe("default base behavior of", function () {
+    describe("isReporterAvailable", function () {
+      it("should report true if the app is found", function () {
+        sandbox.stub(fs, "existsSync").callsFake(() => true);
+        expect(reporter.isReporterAvailable()).to.equal(true);
+      });
+
+      it("should report false if the app is not found", function () {
+        sandbox.stub(fs, "existsSync").callsFake(() => false);
+        expect(reporter.isReporterAvailable()).to.equal(false);
+      });
+
+      it("should only look up on fs once", function () {
+        const fake = sandbox.stub(fs, "existsSync").callsFake(() => false);
+
+        reporter.isReporterAvailable();
+        reporter.isReporterAvailable();
+        reporter.isReporterAvailable();
+
+        expect(fake.callCount).to.equal(1);
+      });
     });
-
-    afterEach(function () {
-        sandbox.restore();
-    });
-
-    describe("default base behavior of", function () {
-        describe("isReporterAvailable", function () {
-            it("should report true if the app is found", function () {
-                sandbox.stub(fs, "existsSync").callsFake(() => true);
-                expect(reporter.isReporterAvailable()).to.equal(true);
-            });
-
-            it("should report false if the app is not found", function () {
-                sandbox.stub(fs, "existsSync").callsFake(() => false);
-                expect(reporter.isReporterAvailable()).to.equal(false);
-            });
-
-            it("should only look up on fs once", function () {
-                const fake = sandbox.stub(fs, "existsSync").callsFake(() => false);
-
-                reporter.isReporterAvailable();
-                reporter.isReporterAvailable();
-                reporter.isReporterAvailable();
-
-                expect(fake.callCount).to.equal(1);
-            });
-        });
-    });
+  });
 });
