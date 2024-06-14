f689e00ec9072c6a76819018e6b0a1f13370fdd6
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-06-14 13:16:53 -0600

    . t better typescript
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- test/Reporting/AggregateDiffReporterTests.ts
+++ test/Reporting/AggregateDiffReporterTests.ts
@@ -1,24 +1,25 @@
-"use strict";
+import {ReporterFactory} from "../../lib/Reporting/ReporterFactory";
 
-var ReporterFactory = require("../../lib/Reporting/ReporterFactory");
-var assert = require("assert");
-var path = require("path");
-var DiffReporterAggregate = require("../../lib/Reporting/DiffReporterAggregate");
+import assert from "assert";
+
+import path from "path";
+
+import DiffReporterAggregate from "../../lib/Reporting/DiffReporterAggregate";
 
 describe("AggregateReporter", function () {
-  var textDiffReporters = ["gitdiff", "p4merge"];
+    const textDiffReporters = ["gitdiff", "p4merge"];
 
-  it("should be able to report on a txt file", function () {
-    this.timeout(60000); // just a test to see if it can pass on AppVeyor?
-    var file = path.join(__dirname, "a.txt");
-    var reporters =
-      ReporterFactory.ReporterFactory.loadAllReporters(textDiffReporters);
-    var reporterX = new DiffReporterAggregate(reporters);
-    var canReportOn = reporterX.canReportOn(file);
-    assert.ok(canReportOn, "Can report on " + file);
+    it("should be able to report on a txt file", function () {
+        this.timeout(60000); // just a test to see if it can pass on AppVeyor?
+        const file = path.join(__dirname, "a.txt");
+        const reporters =
+            ReporterFactory.loadAllReporters(textDiffReporters);
+        const reporterX = new DiffReporterAggregate(reporters);
+        const canReportOn = reporterX.canReportOn(file);
+        assert.ok(canReportOn, "Can report on " + file);
 
-    var foundReporter = reporterX.getReporter(file);
+        const foundReporter = reporterX.getReporter(file);
 
-    assert.ok(foundReporter, "foundReporter not found?");
-  });
+        assert.ok(foundReporter, "foundReporter not found?");
+    });
 });
