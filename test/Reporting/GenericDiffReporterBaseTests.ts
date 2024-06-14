d29812a671b8c9ee3d0195dc6353b03f45cb13f8
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-06-14 13:20:17 -0600

    . t better typescript
    Co-Authored-By: Matt Hughes <221921+mch@users.noreply.github.com>

--- test/Reporting/GenericDiffReporterBaseTests.ts
+++ test/Reporting/GenericDiffReporterBaseTests.ts
@@ -1,44 +1,42 @@
-var fs = require("fs");
-var expect = require("chai").expect;
-//var path = require("path");
-var GenericDiffReporterBase =
-  require("../../lib/Reporting/GenericDiffReporterBase").default;
-var sinon = require("sinon");
+import fs from "fs";
+import {expect} from "chai";
+import GenericDiffReporterBase from "../../lib/Reporting/GenericDiffReporterBase";
+import sinon from "sinon";
 
 describe("GenericDiffReporterBase", function () {
-  var sandbox;
-  var reporter;
+    let sandbox;
+    let reporter;
 
-  beforeEach(function () {
-    sandbox = sinon.createSandbox();
-    reporter = new GenericDiffReporterBase("test");
-  });
-
-  afterEach(function () {
-    sandbox.restore();
-  });
-
-  describe("default base behavior of", function () {
-    describe("isReporterAvailable", function () {
-      it("should report true if the app is found", function () {
-        sandbox.stub(fs, "existsSync").callsFake(() => true);
-        expect(reporter.isReporterAvailable()).to.equal(true);
-      });
-
-      it("should report false if the app is not found", function () {
-        sandbox.stub(fs, "existsSync").callsFake(() => false);
-        expect(reporter.isReporterAvailable()).to.equal(false);
-      });
-
-      it("should only look up on fs once", function () {
-        sandbox.stub(fs, "existsSync").callsFake(() => false);
-
-        reporter.isReporterAvailable();
-        reporter.isReporterAvailable();
-        reporter.isReporterAvailable();
-
-        expect(fs.existsSync.callCount).to.equal(1);
-      });
+    beforeEach(function () {
+        sandbox = sinon.createSandbox();
+        reporter = new GenericDiffReporterBase("test");
     });
-  });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    describe("default base behavior of", function () {
+        describe("isReporterAvailable", function () {
+            it("should report true if the app is found", function () {
+                sandbox.stub(fs, "existsSync").callsFake(() => true);
+                expect(reporter.isReporterAvailable()).to.equal(true);
+            });
+
+            it("should report false if the app is not found", function () {
+                sandbox.stub(fs, "existsSync").callsFake(() => false);
+                expect(reporter.isReporterAvailable()).to.equal(false);
+            });
+
+            it("should only look up on fs once", function () {
+                const fake = sandbox.stub(fs, "existsSync").callsFake(() => false);
+
+                reporter.isReporterAvailable();
+                reporter.isReporterAvailable();
+                reporter.isReporterAvailable();
+
+                expect(fake.callCount).to.equal(1);
+            });
+        });
+    });
 });
