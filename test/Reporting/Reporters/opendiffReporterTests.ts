b1f0b773f326e3f022012206aac505d90ea833b9
Matt Hughes <matt.c.hughes@gmail.com>
2024-09-27 14:30:30 -0600

    . t cleaned up test to actually work
    Co-Authored-By: Llewellyn Falco <10874+isidore@users.noreply.github.com>

--- test/Reporting/Reporters/opendiffReporterTests.ts
+++ test/Reporting/Reporters/opendiffReporterTests.ts
@@ -17,43 +17,11 @@ describe("Reporter", function () {
         var approvedFile = path.join(testDirectory, "Reporting", "Reporters", "a.txt");
         var receivedFile = path.join(testDirectory, "Reporting", "Reporters", "b.txt");
 
-        assert.ok(reporter.report);
-        reporter.report(approvedFile, receivedFile, {
-          spawn: function (command, args) {
-            assert.ok(command.toLowerCase().indexOf("opendiff") >= 0);
-            assert.deepStrictEqual(args, [receivedFile, approvedFile]);
+          const expectedCommand = [receivedFile, approvedFile];
 
-            return {
-              status: 0,
-              stdout: { on: function () {} },
-              stderr: { on: function () {} },
-            };
-          },
-        });
-      });
-
-      xit("reporter args are correct", function () {
-        var reporter = new ReporterUnderTest();
-
-        var approvedFile = path.join(testDirectory, "Reporting", "Reporters", "a.txt");
-        var receivedFile = path.join(testDirectory, "Reporting", "Reporters", "b.txt");
-
-        expect(() => {
-          reporter.report(approvedFile, receivedFile, {
-            spawn: function (command, args) {
-              assert.ok(command.toLowerCase().indexOf("opendiff") >= 0);
-              assert.deepStrictEqual(args, [receivedFile, approvedFile]);
-
-              return {
-                status: 1,
-                stdout: Buffer.from(""),
-                stderr: Buffer.from(
-                  "xcode-select: error: tool 'opendiff' requires Xcode, but active developer directory '/Library/Developer/CommandLineTools' is a command line tools instance",
-                ),
-              };
-            },
-          });
-        }).to.throw(/Could not launch diff tool/);
+          const args = reporter.getCommandArguments(approvedFile, receivedFile);
+          assert.deepEqual(expectedCommand, args.args);
+          assert.ok(args.cmdOptions.toLowerCase().indexOf("opendiff") >= 0);
       });
     });
   }
