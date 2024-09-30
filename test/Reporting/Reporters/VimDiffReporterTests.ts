20f164879d4ed21a267777890b4480cfaf44fe6e
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-30 21:57:57 +0600

    - r converting GenericDiffReporterBase to handle command arguments

--- test/Reporting/Reporters/VimDiffReporterTests.ts
+++ test/Reporting/Reporters/VimDiffReporterTests.ts
@@ -1,29 +1,20 @@
-"use strict";
-
 import {testDirectory} from "../../testPaths";
-
-var expect = require("chai").expect;
-var path = require("path");
-var ReporterUnderTest =
-  require("../../../lib/Reporting/Reporters/vimdiffReporter").default;
+import ReporterUnderTest from "../../../lib/Reporting/Reporters/vimdiffReporter";
+import path from "path";
+import assert from "assert";
 
 describe("Reporter", function () {
   describe("vimdiff", function () {
     it("reporter args are correct", function () {
-      var reporter = new ReporterUnderTest();
+      const reporter = new ReporterUnderTest();
 
-      var approved = path.join(testDirectory, "Reporting", "Reporters", "a.txt");
-      var received = path.join(testDirectory, "Reporting", "Reporters", "b.txt");
+      const approved = path.join(testDirectory, "Reporting", "Reporters", "a.txt");
+      const received = path.join(testDirectory, "Reporting", "Reporters", "r.txt");
 
-      reporter.spawn = (exe, args) => {
-        expect(args).to.deep.equal(["-d", received, approved]);
+      const expectedCommand = ["-d", received, approved];
 
-        return {
-          status: 0, // success
-        };
-      };
-
-      reporter.report(approved, received);
+      const args = reporter.getCommandArguments(approved, received);
+      assert.deepEqual(expectedCommand, args.args);
     });
   });
 });
