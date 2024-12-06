cca8c6b12247b29fdc4f68998d75b92a8a8ad138
Matt Hughes <matt.c.hughes@gmail.com>
2024-12-06 14:24:19 -0700

    . t update gulp-mocha
    Co-Authored-By: Llewellyn Falco <10874+isidore@users.noreply.github.com>

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,20 +1,25 @@
 "use strict";
 
-const gulp = require("gulp");
-const $ = require("gulp-load-plugins")();
+import gulp from "gulp";
+import mocha from 'gulp-mocha';
+import eslint from 'gulp-eslint';
+
+import * as gulp_load_plugins from "gulp-load-plugins";
+
+const $ = gulp_load_plugins;
 
 const paths = {
   mochaTests: ["test/**/*[Tt]ests.js", "test/**/*[Tt]ests.mjs"],
-  filesToLint: ["./lib/**/*.js", "./test/**/*.js", "gulpfile.js"],
+  filesToLint: ["./lib/**/*.js", "./test/**/*.js", "gulpfile.mjs"],
   sourceJSFilesForCodeCoverage: ["./lib/**/*.js"],
 };
 
 gulp.task("lint", function () {
   return gulp
     .src(paths.filesToLint)
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.eslint.failAfterError());
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task(
@@ -28,7 +33,7 @@ gulp.task(
 
 gulp.task("unitTest", function () {
   return gulp.src(paths.mochaTests, { read: false }).pipe(
-    $.mocha({
+    mocha({
       reporter: "spec",
       slow: 500,
       timeout: 5000,
