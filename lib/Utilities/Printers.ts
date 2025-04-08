5cee41f43ecc722c9824b8320740ffd651667646
Ilyas Landikov <ilyas.landikov@icloud.com>
2025-04-08 16:24:11 +0600

    - r compress type parameters

--- lib/Utilities/Printers.ts
+++ lib/Utilities/Printers.ts
@@ -29,42 +29,11 @@ export const EMPTY = [EMPTY_ENTRY];
 type Printer<T> = (...args: T[]) => any;
 type PrinterArgs<T> = T[];
 
-export function printCombinations<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
-  func: (
-    t1: T1,
-    t2: T2,
-    t3: T3,
-    t4: T4,
-    t5: T5,
-    t6: T6,
-    t7: T7,
-    t8: T8,
-    t9: T9,
-  ) => any,
-  params1: T1[],
-  params2: T2[],
-  params3: T3[],
-  params4: T4[],
-  params5: T5[],
-  params6: T6[],
-  params7: T7[],
-  params8: T8[],
-  params9: T9[],
+export function printCombinations<T>(
+  func: Printer<T>,
+  ...args: PrinterArgs<T>
 ): string {
-  const combinations = generateCombinations(
-    [
-      params1,
-      params2,
-      params3,
-      params4,
-      params5,
-      params6,
-      params7,
-      params8,
-      params9,
-    ] as any,
-    [],
-  );
+  const combinations = generateCombinations([...args] as any, []);
 
   let text = "";
   combinations.forEach(
